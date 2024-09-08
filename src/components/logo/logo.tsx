251a0546dc9a6fdd275008a33a04987e28bd213a
navi9000 <icdi7434@gmail.com>
2024-09-08 17:11:38 +0300

    style: logo

--- src/components/logo/logo.tsx
+++ src/components/logo/logo.tsx
@@ -2,5 +2,9 @@ import Link from "next/link"
 
 export default function Logo({ lang }: { lang: string }) {
 
-    return <Link href={`/${lang}`} style={{ marginRight: "auto" }}>МойПроект</Link>
+    return (
+        <Link href={`/${lang}`} style={{ marginRight: "auto" }}>
+            <strong>МойПроект</strong>
+        </Link>
+    )
 }
