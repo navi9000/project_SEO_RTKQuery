ce8e413694face62cae4f7350806496962a8a5a5
navi9000 <icdi7434@gmail.com>
2024-09-08 21:05:20 +0300

    chore: prettier

--- src/components/createModal.tsx
+++ src/components/createModal.tsx
@@ -1,68 +1,92 @@
-import { ChangeEvent, FormEvent, FormEventHandler, useState } from 'react'
-import { createPortal } from 'react-dom'
-import { useGetCategoriesQuery } from '../api/categoryApi'
-import { useCreateItemMutation } from '../api/itemApi'
+import { ChangeEvent, FormEvent, useState } from "react"
+import { createPortal } from "react-dom"
+import { useGetCategoriesQuery } from "../api/categoryApi"
+import { useCreateItemMutation } from "../api/itemApi"
 
 type Props = {
-    isOpen?: boolean
-    close: () => void
+  isOpen?: boolean
+  close: () => void
 }
 
 const initialData = {
-    categoryId: "",
-    price: "",
-    name: ""
+  categoryId: "",
+  price: "",
+  name: "",
 }
 
-export default function CreateModal({
-    isOpen,
-    close
-}: Props) {
+export default function CreateModal({ isOpen, close }: Props) {
+  const [data, setData] = useState(initialData)
+  const categoriesQuery = useGetCategoriesQuery()
+  const [createItem] = useCreateItemMutation()
 
-    const [data, setData] = useState(initialData)
-    const categoriesQuery = useGetCategoriesQuery()
-    const [createItem] = useCreateItemMutation()
-
-    const changeHandler = (e: ChangeEvent<any>) => {
-        if (Object.keys(data).includes(e.target.name)) {
-            setData(prev => ({
-                ...prev,
-                [e.target.name]: e.target.value
-            }))
-        }
+  const changeHandler = (e: ChangeEvent<any>) => {
+    if (Object.keys(data).includes(e.target.name)) {
+      setData((prev) => ({
+        ...prev,
+        [e.target.name]: e.target.value,
+      }))
     }
+  }
 
-    const submitHandler = async (e: FormEvent<HTMLFormElement>) => {
-        e.preventDefault()
-        if (data.categoryId && data.name) {
-            try {
-                const res = await createItem({
-                    ...data,
-                    categoryId: +data.categoryId
-                })
-                if (res) {
-                    setData(initialData)
-                    close()
-                }
-            } catch (err) {
-                console.error('Failed to create a new item')
-            }
+  const submitHandler = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    if (data.categoryId && data.name) {
+      try {
+        const res = await createItem({
+          ...data,
+          categoryId: +data.categoryId,
+        })
+        if (res) {
+          setData(initialData)
+          close()
         }
+      } catch (err) {
+        console.error("Failed to create a new item")
+      }
     }
+  }
 
-    return createPortal(<div className="modal-container">
-        <div className="modal">
-            <button onClick={close}>Закрыть</button>
-            <form autoComplete='off' onSubmit={submitHandler}>
-
-                {!!categoriesQuery.data && <select value={data.categoryId ?? null} name="categoryId" onChange={changeHandler} required>
-                    <option value="" disabled selected hidden>Выберите категорию</option>
-                    {categoriesQuery.data.map(cat => <option key={cat.id} value={cat.id}>{cat.name}</option>)}
-                </select>}
-                <input name="name" placeholder="Наименование" value={data.name} onChange={changeHandler} required />
-                <input name="price" placeholder="Цена" type='number' value={data.price} onChange={changeHandler} required />
-                <button type="submit">Создать</button>
-            </form>
-        </div>
-    </div>, document.body)
+  return createPortal(
+    <div className="modal-container">
+      <div className="modal">
+        <button onClick={close}>Закрыть</button>
+        <form autoComplete="off" onSubmit={submitHandler}>
+          {!!categoriesQuery.data && (
+            <select
+              value={data.categoryId ?? null}
+              name="categoryId"
+              onChange={changeHandler}
+              required
+            >
+              <option value="" disabled selected hidden>
+                Выберите категорию
+              </option>
+              {categoriesQuery.data.map((cat) => (
+                <option key={cat.id} value={cat.id}>
+                  {cat.name}
+                </option>
+              ))}
+            </select>
+          )}
+          <input
+            name="name"
+            placeholder="Наименование"
+            value={data.name}
+            onChange={changeHandler}
+            required
+          />
+          <input
+            name="price"
+            placeholder="Цена"
+            type="number"
+            value={data.price}
+            onChange={changeHandler}
+            required
+          />
+          <button type="submit">Создать</button>
+        </form>
+      </div>
+    </div>,
+    document.body,
+  )
 }
--- src/components/item.tsx
+++ src/components/item.tsx
@@ -4,29 +4,27 @@ import { useUpdateItemMutation } from "../api/itemApi"
 import { useContext } from "react"
 import { CategoryContext } from "../contexts"
 
-export default function TableItem({
-    id,
-    name,
-    categoryId
-}: Item) {
-    const [searchParams] = useSearchParams()
-    const { getById } = useContext(CategoryContext)
-    const [updateItem] = useUpdateItemMutation()
+export default function TableItem({ id, name, categoryId }: Item) {
+  const [searchParams] = useSearchParams()
+  const { getById } = useContext(CategoryContext)
+  const [updateItem] = useUpdateItemMutation()
 
-    const isArchived = searchParams.has("archived")
+  const isArchived = searchParams.has("archived")
 
-    const onClick = () => {
-        updateItem({
-            id,
-            archived: !isArchived
-        })
-    }
+  const onClick = () => {
+    updateItem({
+      id,
+      archived: !isArchived,
+    })
+  }
 
-    return (
-        <li className="item">
-            <div className="item__name">{name}</div>
-            <div className="item__category">{getById(categoryId)}</div>
-            <button className="item__button" onClick={onClick}>{isArchived ? "Восстановить" : "В архив"}</button>
-        </li>
-    )
+  return (
+    <li className="item">
+      <div className="item__name">{name}</div>
+      <div className="item__category">{getById(categoryId)}</div>
+      <button className="item__button" onClick={onClick}>
+        {isArchived ? "Восстановить" : "В архив"}
+      </button>
+    </li>
+  )
 }
