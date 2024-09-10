6560216b9d3a55da466c9930b9d03585ed476e9c
navi9000 <icdi7434@gmail.com>
2024-09-11 00:01:17 +0300

    fix: handle errors

--- helpers/convertNameIntoUrlSnippet.js
+++ helpers/convertNameIntoUrlSnippet.js
@@ -1,55 +1,92 @@
 function transliterate(input) {
-    switch (input) {
-        case "а": return "a"
-        case "б": return "b"
-        case "в": return "v"
-        case "г": return "g"
-        case "д": return "d"
-        case "е": return "e"
-        case "ё": return "yo"
-        case "ж": return "zh"
-        case "з": return "z"
-        case "и": return "i"
-        case "й": return "y"
-        case "к": return "k"
-        case "л": return "l"
-        case "м": return "m"
-        case "н": return "n"
-        case "о": return "o"
-        case "п": return "p"
-        case "р": return "r"
-        case "с": return "s"
-        case "т": return "t"
-        case "у": return "u"
-        case "ф": return "f"
-        case "х": return "h"
-        case "ц": return "ts"
-        case "ч": return "ch"
-        case "ш": return "sh"
-        case "щ": return "scsh"
-        case "ъ": return ""
-        case "ы": return "y"
-        case "ь": return ""
-        case "э": return "e"
-        case "ю": return "u"
-        case "я": return "ya"
-        case " ": return "-"
-        default: return input
-    }
+  switch (input) {
+    case "а":
+      return "a"
+    case "б":
+      return "b"
+    case "в":
+      return "v"
+    case "г":
+      return "g"
+    case "д":
+      return "d"
+    case "е":
+      return "e"
+    case "ё":
+      return "yo"
+    case "ж":
+      return "zh"
+    case "з":
+      return "z"
+    case "и":
+      return "i"
+    case "й":
+      return "y"
+    case "к":
+      return "k"
+    case "л":
+      return "l"
+    case "м":
+      return "m"
+    case "н":
+      return "n"
+    case "о":
+      return "o"
+    case "п":
+      return "p"
+    case "р":
+      return "r"
+    case "с":
+      return "s"
+    case "т":
+      return "t"
+    case "у":
+      return "u"
+    case "ф":
+      return "f"
+    case "х":
+      return "h"
+    case "ц":
+      return "ts"
+    case "ч":
+      return "ch"
+    case "ш":
+      return "sh"
+    case "щ":
+      return "shch"
+    case "ъ":
+      return ""
+    case "ы":
+      return "y"
+    case "ь":
+      return ""
+    case "э":
+      return "e"
+    case "ю":
+      return "u"
+    case "я":
+      return "ya"
+    case " ":
+      return "-"
+    default:
+      return input
+  }
 }
 
 function convertNameIntoUrlSnippet(input) {
-    return input.split("").map(symbol => transliterate(symbol)).join("")
+  return input
+    .split("")
+    .map((symbol) => transliterate(symbol))
+    .join("")
 }
 
 function trimIdFromSnippet(input) {
-
-    const splitUrl = input.split("-")
-    splitUrl.pop()
-    return splitUrl.join("-")
+  const splitUrl = input.split("-")
+  splitUrl.pop()
+  return splitUrl.join("-")
 }
 
 module.exports = {
-    convertNameIntoUrlSnippet,
-    trimIdFromSnippet
+  convertNameIntoUrlSnippet,
+  trimIdFromSnippet
 }
--- index.js
+++ index.js
@@ -1,215 +1,243 @@
-const express = require('express')
-const bodyParser = require('body-parser')
+const express = require("express")
+const bodyParser = require("body-parser")
 const { Sequelize, Model, DataTypes, Op } = require("sequelize")
-const { convertNameIntoUrlSnippet, trimIdFromSnippet } = require('./helpers/convertNameIntoUrlSnippet')
+const {
+  convertNameIntoUrlSnippet,
+  trimIdFromSnippet
+} = require("./helpers/convertNameIntoUrlSnippet")
 
 const sequelize = new Sequelize({
-    dialect: "sqlite",
-    storage: "./db/db.sqlite"
+  dialect: "sqlite",
+  storage: "./db/db.sqlite"
 })
 
-class Category extends Model { }
+class Category extends Model {}
 
-Category.init({
+Category.init(
+  {
     id: {
-        type: DataTypes.INTEGER,
-        autoIncrement: true,
-        primaryKey: true
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true
     },
     name: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        unique: true
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true
     }
-}, { sequelize, modelName: 'category' })
+  },
+  { sequelize, modelName: "category" }
+)
 
-class Item extends Model { }
+class Item extends Model {}
 
-Item.init({
+Item.init(
+  {
     id: {
-        type: DataTypes.INTEGER,
-        autoIncrement: true,
-        primaryKey: true,
-        unique: true
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+      unique: true
     },
     categoryId: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        references: {
-            model: Category,
-            key: 'id',
-        }
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: Category,
+        key: "id"
+      }
     },
     name: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        unique: true
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true
     },
     price: {
-        type: DataTypes.DECIMAL,
-        allowNull: false
+      type: DataTypes.DECIMAL,
+      allowNull: false
     },
     urlSnippet: {
-        type: DataTypes.STRING,
-        allowNull: false
+      type: DataTypes.STRING,
+      allowNull: false
     },
     archived: {
-        type: DataTypes.BOOLEAN,
-        defaultValue: false
-    },
-}, { sequelize, modelName: 'item' })
+      type: DataTypes.BOOLEAN,
+      defaultValue: false
+    }
+  },
+  { sequelize, modelName: "item" }
+)
 
 Category.hasMany(Item)
 Item.belongsTo(Category)
 
 sequelize.sync()
 
-
 const app = express()
 const port = 8080
 
 // Middleware for parsing request body
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
 app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, authorization");
-    res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
-    next();
-});
-
-
-app.get('/items', async (req, res) => {
-
-    const { archived, sortBy, search } = req.query
-    const order = sortBy?.split(":") ?? undefined
-
-    const data = await Item.findAll({
-        where: {
-            archived: archived === "true",
-            name: {
-                [Op.substring]: search ?? ""
-            }
-        },
-        order: order ? [order] : undefined
-    })
-    res.json({
-        is_success: true,
-        data
-    })
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, authorization"
+  )
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE")
+  next()
 })
 
-app.get('/item', async (req, res) => {
+app.get("/items", async (req, res) => {
+  const { archived, sortBy, search } = req.query
+  const order = sortBy?.split(":") ?? undefined
 
-    const { urlSnippet } = req.query
+  const data = await Item.findAll({
+    where: {
+      archived: archived === "true",
+      name: {
+        [Op.substring]: search ?? ""
+      }
+    },
+    order: order ? [order] : undefined
+  })
+  res.json({
+    is_success: true,
+    data
+  })
+})
 
-    const data = await Item.findOne({
-        where: {
-            urlSnippet: trimIdFromSnippet(urlSnippet)
-        }
-    })
+app.get("/item", async (req, res) => {
+  const { urlSnippet } = req.query
 
-    if (data) {
-        res.json({
-            is_success: true,
-            data
-        })
-    } else {
-        res.status(404).json({
-            is_success: false,
-            message: "Item not found"
-        })
+  const data = await Item.findOne({
+    where: {
+      urlSnippet: trimIdFromSnippet(urlSnippet)
     }
+  })
+
+  if (data) {
+    res.json({
+      is_success: true,
+      data
+    })
+  } else {
+    res.status(404).json({
+      is_success: false,
+      message: "Item not found"
+    })
+  }
 })
 
-app.post('/items', async (req, res) => {
+app.post("/items", async (req, res) => {
+  try {
     const data = await Item.create({
-        ...req.body,
-        urlSnippet: convertNameIntoUrlSnippet(req.body.name)
+      ...req.body,
+      urlSnippet: convertNameIntoUrlSnippet(req.body.name)
     })
     res.json({
-        is_success: true,
-        data
+      is_success: true,
+      data
     })
-})
-
-app.put('/items/:id', async (req, res) => {
-    const data = await Item.findByPk(req.params.id)
-    if (data) {
-        await data.update(req.body)
-        setTimeout(() => {
-            res.json({
-                is_success: true,
-                data
-            })
-        }, 3000)
-    } else {
-        res.status(404).json({
-            is_success: false,
-            message: "Item not found"
-        })
-    }
-})
-
-app.get('/categories', async (req, res) => {
-    const data = await Category.findAll()
-    res.json({
-        is_success: true,
-        data
+  } catch {
+    res.status(400).json({
+      is_success: false,
+      message: "Invalid data"
     })
+  }
 })
 
-app.post('/categories', async (req, res) => {
-    const data = await Category.create({
-        ...req.body,
-    })
-    res.json({
-        is_success: true,
-        data
-    })
-})
-
-app.get('/users/:id', async (req, res) => {
-    const id = +req.params.id
-    if (id === 0 || isNaN(id)) {
-        res.status(400).json({
-            is_success: false,
-            message: 'Bad request'
-        })
-    } else {
-        setTimeout(() => {
-            res.json({
-                is_success: true,
-                data: {
-                    id: 1,
-                    name: 'admin',
-                    role_id: 1
-                }
-            })
-        }, 2000)
-    }
-
-})
-
-app.get('/roles/:id', async (req, res) => {
-    const id = req.params.id
-    if (id !== "1") {
-        res.status(400).json({
-            is_success: false,
-            message: 'Bad request'
-        })
-    } else {
+app.put("/items/:id", async (req, res) => {
+  const data = await Item.findByPk(req.params.id)
+  if (data) {
+    try {
+      await data.update(req.body)
+      setTimeout(() => {
         res.json({
-            is_success: true,
-            data: {
-                id: 1,
-                name: 'Admin'
-            }
+          is_success: true,
+          data
         })
+      }, 3000)
+    } catch {
+      res.status(400).json({
+        is_success: false,
+        message: "Invalid data"
+      })
     }
+  } else {
+    res.status(404).json({
+      is_success: false,
+      message: "Item not found"
+    })
+  }
+})
+
+app.get("/categories", async (req, res) => {
+  const data = await Category.findAll()
+  res.json({
+    is_success: true,
+    data
+  })
+})
+
+app.post("/categories", async (req, res) => {
+  try {
+    const data = await Category.create({
+      ...req.body
+    })
+    res.json({
+      is_success: true,
+      data
+    })
+  } catch {
+    res.status(400).json({
+      is_success: false,
+      message: "Invalid data"
+    })
+  }
+})
+
+app.get("/users/:id", async (req, res) => {
+  const id = +req.params.id
+  if (id === 0 || isNaN(id)) {
+    res.status(400).json({
+      is_success: false,
+      message: "Bad request"
+    })
+  } else {
+    setTimeout(() => {
+      res.json({
+        is_success: true,
+        data: {
+          id: 1,
+          name: "admin",
+          role_id: 1
+        }
+      })
+    }, 2000)
+  }
+})
+
+app.get("/roles/:id", async (req, res) => {
+  const id = req.params.id
+  if (id !== "1") {
+    res.status(400).json({
+      is_success: false,
+      message: "Bad request"
+    })
+  } else {
+    res.json({
+      is_success: true,
+      data: {
+        id: 1,
+        name: "Admin"
+      }
+    })
+  }
 })
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port ${port}`)
 })
